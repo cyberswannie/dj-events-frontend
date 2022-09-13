06b8a765b64c1680c6618d3f70ec890088de233a
James Swann <cyberswannie@hotmail.com>
2022-09-13 18:49:57 +0100

    First commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout"
+export default function about() {
+  return (
+    <Layout title="Information About Our Events">
+        <h1>About</h1>
+        <p>This ia an app to find the latest DJ and other musical events</p>
+        <p>Version 1.0.0</p>
+    </Layout>
+  )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout"
+
+export default function AddEventPage() {
+  return (
+    <Layout>
+        <h1>Add new event</h1>
+    </Layout>
+  )
+}
